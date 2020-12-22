a7013ed29e0e87fc139af0fb753d552c33fac3a9
ybc <answerybc@163.com>
2020-12-22 18:48:31 +0800

    rewrite

--- SL.c
+++ SL.c
@@ -2,191 +2,238 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define OK 1;
-#define ERROR -1;
-#define OVERFLOW -2;
+#define OK 1
+#define ERROR -1
+#define OVERFLOW -2
 
-typedef int Status;
+typedef int STATUS;
 
-const int LIST_SIZE=20;
+const int LIST_SIZE = 20;
+const int NAME_LEN = 20;
+
+static int class_size;
+
 //学生结构体
 typedef struct{
-	int num; 
-	char name[20];
-
-}stu;
+	int no; //学号
+	char name[NAME_LEN];
+}student;
 
 //顺序表结构体
 typedef struct{
-	stu *elem;
+	student *elem;
 	int length;
 	int ListSize;
-
 }sqlist;
+
+
+void s_gets(char *str, int len)
+{
+    fgets(str, len, stdin);
+    char *find;
+    if ((find = strchr(str, '\n')) != NULL) {
+        //把字符串最后的换行符换成空字符
+        *find = '\0';
+    } else {
+        //处理调缓冲区的剩余字符
+        while (getchar() != '\n')
+            continue;
+    }
+}
 
 /**
 *初始化顺序表
 **/
-Status  InitList(sqlist *L){
-	if(L==NULL){
+STATUS  initList(sqlist *L) {
+	if (L == NULL) {
 		return ERROR;
 	}
-
-	L->elem=(stu *)malloc(sizeof(stu)*LIST_SIZE);
-	L->length=0;
-	L->ListSize=LIST_SIZE;
-
+	//todo 一开始不分配空间
+	L->elem = (student *) malloc(sizeof (student) * LIST_SIZE);
+    L->length = 0;
+    L->ListSize = LIST_SIZE;
 	return OK;
-
 }
 
 /**
 * 销毁顺序表
 */
-
-Status DesList(sqlist *L ){
-	if(L->elem){
-		free(L->elem);}
-		L->elem=NULL;
-	
-	return OK;
+STATUS destructList(sqlist *L )
+{
+    if (L->elem) {
+        free(L->elem);
+    }
+    L->elem = NULL; //把elem指向null
+    return OK;
 }
 
 
 /**
 *  录入学生信息
-*
 */
+STATUS initStudentList(sqlist *L)
+{
+    extern int class_size;
+    printf("输入班级学生的总数量:");
+    if (!scanf("%d", &class_size)){
+        class_size = LIST_SIZE;
+    }
 
-void Insert(sqlist *L,int n){
-	int i;
-	printf("请依次输入学生信息");
-	for(i=0;i<n;i++){
-		printf("第%d个学生信息：\n学号：",i+1);
-		scanf("%d",&L->elem[i].num);
-		printf("姓名：");
-		scanf("%s",L->elem[i].name);
-	}
-	L->length=n;
+	printf("请依次输入学生信息: \n");
+    for (int i = 0; i < class_size; i++) {
+        printf("第%d个学生信息：\n学号：", i+1);
+        scanf("%d", &(L->elem[i].no));
+        fflush(stdin);
+        printf("姓名：");
+        s_gets(L->elem[i].name, NAME_LEN);
+    }
+	L->length = class_size;
+    return OK;
 }
 
 
 /**
 *查找学生是否存在
 */
-Status Locate(sqlist L,int n){
-	int i=1;
-	//return L.elem[2].num;
-	while(i<=L.length&&n!=L.elem[i-1].num){
-		i++;
-	}
-	if(i>L.length){
-		return 0;
-	}else return i;
+int findStu(sqlist L)
+{
+    int no;
+    printf("输入要查找的学生学号:");
+    scanf("%d", &no);
+    int i = 0;
+    while (i < L.length && no != L.elem[i].no)
+        i++;
 
+    if (i > L.length)
+        return ERROR;
+
+    return i;
 }
 
 
 /**
-*插入一条学生信息
-*
-*/
-Status Insert_stu(sqlist *L,int i,stu s){
-	int n=L->length-1;
+ *
+ * @param L
+ * @param position 要插入的位置
+ * @return
+ */
+STATUS insertNewStu(sqlist *L)
+{
+    int position;
+    printf("输入要插入的位置:");
+    scanf("%d", &position);
+    student stu;
+    printf("输入学号:");
+    scanf("%d", &stu.no);
+    printf("输入姓名:");
+    fflush(stdin);
+    s_gets(stu.name, NAME_LEN);
+
 	//合法性检查
+	if (position < 1 || position > L->ListSize) {
+	    printf("位置不合法 \n");
+	    return ERROR;
+	}
+	if (L->length >= L->ListSize) {
+	    printf("学员数量已满 \n");
+	    return OVERFLOW;
+	}
 
-	if(i<1||i>L->length+1){
-		return ERROR;	
+	for (int n = L->length-1; n >= position - 1; n--) {
+	    //移动插入位置之后的所有元素
+		L->elem[n+1] = L->elem[n];
 	}
-	if(L->length>=LIST_SIZE){
-		return OVERFLOW;
-	 }
-	
-	for(;n>=i-1;n--){
-		L->elem[n+1]=L->elem[n];		
-	}
-	L->elem[i-1]=s;
+
+	L->elem[position-1] = stu;
 	L->length++;
-
 	return OK;
-
 }
 
 
 /**
-*删除一条学生信息
-*/
-Status Del_stu(sqlist *L,int i){
-	int q=i-1;
-	if(L->length==0||i<1||i>L->length)
-	{  return ERROR;}
+ * 删除学员
+ * @param L
+ * @return
+ */
+STATUS delStu(sqlist *L)
+{
+    int no;
+    printf("输入要删除学生的学号:");
+    scanf("%d", &no);
+    int position = 0;
+    while (position<L->length && L->elem[position].no != no)
+        position++;
 
-	for(q;q<L->length-1;q++){
-		L->elem[q]=L->elem[q+1];
-	}
-	L->length--;
-	return OK;
+    if (position >= L->length)
+        return ERROR;
 
+    for (int q = position; q < L->length - 1; q++) {
+        L->elem[q] = L->elem[q + 1];
+    }
+
+    L->length--;
+    return OK;
 }
 
-
 /**
-*输出所有学生信息
-*/
-void show_stu(sqlist L){
-	int i=0;
-	if(L.length<1){
+ * 输出所有学员信息
+ * @param L
+ */
+void showStu(sqlist L)
+{
+	if (L.length<1) {
 		printf("学生信息为空!\n");
 		return;
 	}
-	for(;i<L.length;i++){
-		printf("第%d名学生：姓名%s  学号：%d \n",i+1,L.elem[i].name,L.elem[i].num);
-	
+	for(int i=0; i<L.length; i++) {
+		printf("第%d名学生：姓名%s  学号：%d \n",i+1,L.elem[i].name,L.elem[i].no);
 	}
-
-
 }
 
 
-
-typedef struct{
-	int sz[10];
-	int number;
-	int age;
-}cla;
-int main(){
-
-	int m;	stu ns;int j; int k;int h=1;
+int main()
+{
+	int h=1;
+	int op;
 	sqlist L;
-	InitList(&L);
-	printf("***欢迎使用学生信息管理系统***\n");
-
-	while(h==1){
-		printf("请选择您要执行的操作：\n");
-		printf("输入学生信息[1]；插入学生信息[2] ；\n查找学生信息[3]；输出所有学生信息[4]；\n删除学生信息[5]；退出 [6]\n");
-		scanf("%d",&m);
-		switch(m){
-		case 1:printf("学生人数：");scanf("%d",&j);
-			   if(j>LIST_SIZE){
-					printf("学生人数超过限制\n");	
-					break;
-				}
-			   Insert(&L,j);break;
-			case 2:
-					ns.num=20;
-					strcpy(ns.name,"yyy");
-					Insert_stu(&L,3,ns);break;
-			case 3: printf("输入要查找的学生学号"); scanf("%d",&k);printf("%d",Locate(L,k));break;
-			case 4:show_stu(L);break;
-			case 5:printf("输入要删除学生的学号：");scanf("%d",&k);Del_stu(&L,k);break;
-			case 6:	DesList(&L);h=-1;break;
-			default:DesList(&L);h=-1;break;
-		}
-	
+	if (!initList(&L)) {
+	    printf("初始化学生结构失败 \n");
+	    exit(EXIT_FAILURE);
 	}
 
-
-	
-	system("pause");
+	printf("***欢迎使用学生信息管理系统***\n");
+	while (h == 1) {
+		printf("请选择您要执行的操作：\n");
+		printf("输入学生信息[1]；插入学生信息[2] ；\n查找学生信息[3]；输出所有学生信息[4]；\n删除学生信息[5]；退出 [6]\n");
+		scanf("%d", &op);
+		switch (op) {
+            case 1:
+                initStudentList(&L);
+                break;
+            case 2:
+                insertNewStu(&L);
+                break;
+			case 3:
+            {
+                int position = findStu(L);
+                if (position != ERROR) {
+                    printf("姓名:%s \n", L.elem[position].name);
+                } else {
+                    printf("not found \n");
+                }
+            }
+			    break;
+			case 4:
+			    showStu(L);
+			    break;
+			case 5:
+			    delStu(&L);
+			    break;
+            default:
+                destructList(&L);
+			    h=-1;
+			    break;
+		}
+	}
 	return 0;
 }
